fbe056d2ee93a70d7d602366cac4b6a532a483b7
iqbalj <iqballabqi127@gmail.com>
2026-06-20 11:17:50 +0700

    feat(routing): add public.urls for home, about, services, contact pages

---
+++ public/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("services/", services, name="services"),
+    path("about/", about, name="about"),
+    path("contact", contact, name="contact"),
+]
