0ac0c1f40862907ddfbbe9108fc5b2b42ae9df88
iqbalj <iqballabqi127@gmail.com>
2026-06-20 11:17:42 +0700

    refactor(context): rename context processor from site_settings to site

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "public.context_processors.site_settings"
+                "public.context_processors.site"
             ],
         },
     },
--- public/context_processors.py
+++ public/context_processors.py
@@ -1,7 +1,20 @@
 from .models import SiteSetting
 
-def site_settings(request):
-    site = SiteSetting.objects.first()
-    return {
-        "site": site
-    }
+
+def site(request):
+    """
+    Tambahkan ke TEMPLATES[0]['OPTIONS']['context_processors'] di settings.py
+    supaya {{ site }} tersedia di semua template.
+
+    Contoh pakai di template:
+        {{ site.company_name }}
+        {{ site.full_address }}
+        {{ site.operating_hours_display }}
+        <a href="{{ site.whatsapp_link }}">WhatsApp</a>
+    """
+    try:
+        site_setting = SiteSetting.load()
+    except SiteSetting.DoesNotExist:
+        site_setting = None
+
+    return {"site": site_setting}
